5c0c5aca1d0aa24de9a16b978aee908c6d0d40c6
PriBoris <prishchepenko.boris@gmail.com>
2017-04-29 12:48:45 +0300

    Initialize terminal blocks

--- AltiumLibrary2/DataBaseTables/XlsMerger.py
+++ AltiumLibrary2/DataBaseTables/XlsMerger.py
@@ -1,10 +1,6 @@
 
 
 
-def console_friendly(text):
-    import sys
-    return text.encode('utf8').decode(sys.stdout.encoding)
-
 print("XlsMerger")
 print()
 
@@ -27,38 +23,44 @@ read_file_count = 0
 read_sheet_count = 0
 
 
-for read_file_name in os.listdir(os.getcwd()):
+with open("XlsMerger.txt", "w") as text_file:
 
-    if read_file_name.endswith(".xls") and not read_file_name == write_file_name:
+    for read_file_name in os.listdir(os.getcwd()):
 
-        print()
-        print("info:", "reading file \'{0}\'".format(console_friendly(read_file_name)))
+        if read_file_name.endswith(".xls") and not read_file_name == write_file_name:
 
-        read_file_count += 1
-        read_book = xlrd.open_workbook(read_file_name,formatting_info=True)
+            text_file.write("\n")
+            text_file.write("info: reading file \'{0}\'\n".format(read_file_name))
 
-        for sheet_index in range(len(read_book.sheets())):
+            read_file_count += 1
+            read_book = xlrd.open_workbook(read_file_name,formatting_info=True)
 
-            read_sheet = read_book.sheet_by_index(sheet_index)
-            read_sheet_count += 1
-            read_sheet_name = read_sheet.name
+            for sheet_index in range(len(read_book.sheets())):
 
-            print("info:", "reading sheet \'{0}\' ({1} rows)".format(console_friendly(read_sheet_name), read_sheet.nrows))
+                read_sheet = read_book.sheet_by_index(sheet_index)
+                read_sheet_count += 1
+                read_sheet_name = read_sheet.name
 
-            write_sheet = write_book.add_sheet(read_file_name.split(".xls")[0] + " - " + read_sheet_name)
-            
+                text_file.write("info: reading sheet \'{0}\' ({1} rows)\n".format(read_sheet_name, read_sheet.nrows))
 
-            print("info:", "cells per row = ", [len(read_sheet.row_values(row)) for row in range(read_sheet.nrows)])
+                write_sheet_name = read_file_name.split(".xls")[0] + " - " + read_sheet_name
 
-            for row_index in range(read_sheet.nrows):
-                for col_index, read_value in zip(range(len(read_sheet.row_values(row_index))), read_sheet.row_values(row_index)):
-                    write_sheet.write(row_index, col_index, read_value)
+                text_file.write("info: writing sheet \'{0}\'\n".format(write_sheet_name))
 
+                write_sheet = write_book.add_sheet(write_sheet_name)
+                
+
+                text_file.write("info: cells per row = {0}\n".format([len(read_sheet.row_values(row)) for row in range(read_sheet.nrows)]))
+
+                for row_index in range(read_sheet.nrows):
+                    for col_index, read_value in zip(range(len(read_sheet.row_values(row_index))), read_sheet.row_values(row_index)):
+                        write_sheet.write(row_index, col_index, read_value)
+
+                pass
             pass
         pass
     pass
 pass
-
 
 
 write_book.save(write_file_name)
